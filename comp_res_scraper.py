90fdbf0cffd6c7b41cba36abc741495f29124e14
rkrabek <rkrabek@hotmail.com>
2016-04-19 23:35:19 +0000

    split off shared functions into helpers file

--- comp_res_scraper.py
+++ comp_res_scraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import urllib2
 import comp_res_references
+import helpers
 import requests
 import re
 import pickle
@@ -59,83 +60,6 @@ def validate_age_dances(curr_event):
     else:
         return True
 
-# finds level of event
-def get_level(event):
-    level = ""
-    newcomer_try = re.findall("((Pre Bronze)|(Pre-Bronze))", event)
-    if len(newcomer_try) != 0:
-        level = "Newcomer"
-    else:
-        level_try = re.findall("((Newcomer)|(Bronze)|(Silver)|(Gold)|(Novice)|(Championship))", event)
-        if len(level_try) == 0:
-            level_try = re.findall("((Beginner)|(Intermediate)|(Advanced)|(Syllabus)|(Open)|(Pre-Champ))", event)
-            if len(level_try) == 0:
-                print "Level not found in" + event
-                level = event
-            else:
-                level = level_try[-1][0]
-            if level == "Pre-Champ":
-                level = "PreChamp"
-            elif level == "Beginner" or level == "Syllabus":
-                level = "Bronze"
-            elif level == "Intermediate":
-                level = "Silver"
-            elif level == "Advanced":
-                level = "Gold"
-            elif level == "Open":
-                level = "Championship"
-        else:
-            level = level_try[0][0]
-    return level
-
-# finds dances of event
-def get_dances_abbr(event):
-    start = event.rfind("(")
-    end = event.rfind(")")
-    dances_abbr = re.findall("([WTVFQCRSJPMB])", event[start:end])
-    return dances_abbr
-
-# returns style and full name of dances
-def get_style_dances(event):
-    dances_abbr = get_dances_abbr(event)
-    style = ""
-    style_try = re.findall("((Standard)|(Latin)|(Rhythm)|(Smooth))", event)
-    if len(style_try) == 0:
-        style_try = re.findall("((Intl\.|Intl|Am\.|Am|Ballroom|BALLROOM|LATIN))", event)
-        if len(style_try) == 0:
-            print "Style not found in" + event
-            style = event
-        else:
-            style = style_try[-1][0]
-        if style in ["Intl.", "Intl"]:
-            if any(x in dances_abbr for x in ['C','S','R','P','J']):
-                style = "Latin"
-            else:
-                style = "Standard"
-        elif style in ["Am.", "Am"]:
-            if any(x in dances_abbr for x in ['W','T','F','V']):
-                style = "Smooth"
-            else:
-                style = "Rhythm"
-        elif style in ["Ballroom", "BALLROOM"]:
-            style = "Standard"
-        elif style == "LATIN":
-            style = "Latin"
-    else:
-        style = style_try[0][0]
-    if style in comp_res_references.style_list:
-        # print event
-        # event_list.write(event + '\n')
-        try:
-            dances = [comp_res_references.dance_map[style][x] for x in dances_abbr]
-        except KeyError:
-            dances = []
-            # event_list.write(event + " has dances in it which do not match the detected style \n")
-            print event + " has dances in it which do not match the detected style"
-    else:
-        dances = []
-    return (style, dances)
-
 # finds number of heats
 def get_heats(heats):
     if heats:
@@ -192,8 +116,8 @@ def get_ycn_res(competition):
         if len(event) != 0:
             curr_event = event[0].text
             if validate_age_dances(curr_event):
-                curr_level = get_level(curr_event)
-                style_dances = get_style_dances(curr_event)
+                curr_level = helpers.get_level(curr_event)
+                style_dances = helpers.get_style_dances(curr_event)
                 curr_style = style_dances[0]
                 curr_dances = style_dances[1]
                 # print curr_level
--- entries_scraper.py
+++ entries_scraper.py
@@ -3,30 +3,31 @@ import urllib2
 import requests
 import re
 import pickle
-import individual_res5
+import helpers
+import comp_res_references
 
 competitors = {}
 pf_in = open("comp_res.p", "rb")
-print "loading"
+print "loading pickle file"
 competitors = pickle.load(pf_in)
 print "done loading"
-# competitors['RobinGanek'] = individual_res5.get_ycn("Ganek","Robin")
 
 def check_eligibility_pts(name, level, style, dance):
     def pt_lookup(level):
         try:
             return competitors[name].levels.__dict__[level].__dict__[style].__dict__[dance]
         except KeyError:
+            # There is no record of eligible ycn placement history in pickle file
             return 0
-    l_index = level_list.index(level)
+    l_index = comp_res_references.level_list.index(level)
     curr_lvl_pts = pt_lookup(level)
     if l_index > 5:
         return (True, 0)
     else:
-        pt_trickle = (curr_lvl_pts + pt_lookup(level_list[l_index+1]))
+        pt_trickle = (curr_lvl_pts + pt_lookup(comp_res_references.level_list[l_index+1]))
         if l_index < 5:
             if pt_trickle < 7:
-                if (pt_lookup(level_list[l_index+2]) != 0):
+                if (pt_lookup(comp_res_references.level_list[l_index+2]) != 0):
                     return (False, pt_trickle + 7)
                 else:
                     return (True, pt_trickle)
@@ -34,37 +35,7 @@ def check_eligibility_pts(name, level, style, dance):
                 return (False, pt_trickle)
         else:
             return ((pt_trickle < 7), pt_trickle)
-level_list = ["Newcomer","Bronze","Silver","Gold","Novice","PreChamp","Championship"]
 
-dance_map = {
-    "Latin" : {
-        "S" : "Samba",
-        "C" : "ChaCha",
-        "R" : "Rumba",
-        "J" : "Jive",
-        "P" : "PasoDoble"
-    },
-    "Standard" : { 
-        "W" : "Waltz",
-        "T" : "Tango",
-        "V" : "VienneseWaltz",
-        "F" : "Foxtrot",
-        "Q" : "Quickstep",
-    },
-    "Rhythm" : {
-        "C" : "ChaCha",
-        "R" : "Rumba",
-        "S" : "Swing",
-        "M" : "Mambo",
-        "B" : "Bolero"
-    },
-    "Smooth" : {
-        "W" : "Waltz",
-        "T" : "Tango",
-        "V" : "VienneseWaltz",
-        "F" : "Foxtrot"
-    }
-}
 def entries_post(event):
 
     url = "http://entries.o2cm.com/default.asp"
@@ -97,65 +68,65 @@ def entries_post(event):
     entries = soup.find_all('td', {"class":["h5b", "h5n"]})
     return entries
 
-def get_entry_level(header):
-    level = ""
-    level_try = re.findall("((Newcomer)|(Bronze)|(Silver)|(Gold)|(Novice)|(Championship))", header)
-    if len(level_try) == 0:
-        level_try = re.findall("((Beginner)|(Intermediate)|(Advanced)|(Syllabus)|(Open)|(Pre-Champ))", header)
-        if len(level_try) == 0:
-            print "Level not found in" + header
-            level = header
-        else:
-            level = level_try[0][0]
-        if level == "Pre-Champ":
-            level = "PreChamp"
-        elif level == "Beginner" or level == "Syllabus":
-            level = "Bronze"
-        elif level == "Intermediate":
-            level = "Silver"
-        elif level == "Advanced":
-            level = "Gold"
-        elif level == "Open":
-            level = "Championship"
-    else:
-        level = level_try[0][0]
-    return level
+# def get_entry_level(header):
+#     level = ""
+#     level_try = re.findall("((Newcomer)|(Bronze)|(Silver)|(Gold)|(Novice)|(Championship))", header)
+#     if len(level_try) == 0:
+#         level_try = re.findall("((Beginner)|(Intermediate)|(Advanced)|(Syllabus)|(Open)|(Pre-Champ))", header)
+#         if len(level_try) == 0:
+#             print "Level not found in" + header
+#             level = header
+#         else:
+#             level = level_try[0][0]
+#         if level == "Pre-Champ":
+#             level = "PreChamp"
+#         elif level == "Beginner" or level == "Syllabus":
+#             level = "Bronze"
+#         elif level == "Intermediate":
+#             level = "Silver"
+#         elif level == "Advanced":
+#             level = "Gold"
+#         elif level == "Open":
+#             level = "Championship"
+#     else:
+#         level = level_try[0][0]
+#     return level
 
-def get_entry_dances(header):
-    start = header.find("(")
-    end = header.find(")")
-    dances = re.findall("([WTVFQCRSJPMB])", header[start:end])
-    return dances
+# def get_entry_dances(header):
+#     start = header.find("(")
+#     end = header.find(")")
+#     dances = re.findall("([WTVFQCRSJPMB])", header[start:end])
+#     return dances
 
-def get_entry_style(entry, dances):
-    style = ""
-    dances_expanded = []
-    style_try = re.findall("((Standard)|(Latin)|(Rhythm)|(Smooth))", entry)
-    if len(style_try) == 0:
-        style_try = re.findall("((Intl\.|Am\.|Ballroom))", entry)
+# def get_entry_style(entry, dances):
+#     style = ""
+#     dances_expanded = []
+#     style_try = re.findall("((Standard)|(Latin)|(Rhythm)|(Smooth))", entry)
+#     if len(style_try) == 0:
+#         style_try = re.findall("((Intl\.|Am\.|Ballroom))", entry)
         
-        if len(style_try) == 0:
-            print "Style not found in" + entry
-            style = entry
-        else:
-            style = style_try[0][0]
-        if style == "Intl.":
-            if any(x in dances for x in ['C','S','R','P','J']):
-                style = "Latin"
-            else:
-                style = "Standard"
-        elif style == "Am.":
-            if any(x in dances for x in ['W','T','F','V']):
-                style = "Smooth"
-            else:
-                style = "Rhythm"
-        elif style == "Ballroom":
-            style = "Standard"
-    else:
-        style = style_try[0][0]
-        # print style
-    dances = [dance_map[style][x] for x in dances]
-    return (style, dances)
+#         if len(style_try) == 0:
+#             print "Style not found in" + entry
+#             style = entry
+#         else:
+#             style = style_try[0][0]
+#         if style == "Intl.":
+#             if any(x in dances for x in ['C','S','R','P','J']):
+#                 style = "Latin"
+#             else:
+#                 style = "Standard"
+#         elif style == "Am.":
+#             if any(x in dances for x in ['W','T','F','V']):
+#                 style = "Smooth"
+#             else:
+#                 style = "Rhythm"
+#         elif style == "Ballroom":
+#             style = "Standard"
+#     else:
+#         style = style_try[0][0]
+#         # print style
+#     dances = [comp_res_references.dance_map[style][x] for x in dances]
+#     return (style, dances)
 
 event = ""
 level = ""
@@ -173,9 +144,8 @@ for post in entries_post("mit"):
                     print name_concat + " has placed out of " + event + " with " + str(elig_true[1]) + " points in " + level + " " + dance
     else:
         event = post.text
-        level = get_entry_level(event)
-        dances_abrv = get_entry_dances(event)
-        sty_dan = get_entry_style(event, dances_abrv)
+        level = helpers.get_level(event)
+        sty_dan = helpers.get_style_dances(event)
         style = sty_dan[0]
         dances = sty_dan[1]
         # print '\n'+ event
